9fc2f611b2665383eeacd7cf7f9326d4ec0e98af
Piet Goris <piet.goris@gmail.com>
2023-10-14 20:49:14 +0200

    Added proper walking

--- Player.gd
+++ Player.gd
@@ -7,13 +7,16 @@ var target_object;
 
 var MAX_SPEED = 80;
 var velocity = Vector2.ZERO;
+var last_direction = Vector2.DOWN;
 var moving = false;
+var current_animation;
 
 var inventory = [preload("res://items/rusted_key.tres")];
 var selected_item = null;
 
 onready var camera = $Camera2D;
 onready var navigation_agent = $NavigationAgent2D;
+onready var sprite = $AnimatedSprite;
 
 func _init():
 	add_to_group(Groups.PLAYER);
@@ -43,19 +46,64 @@ func set_target_position(pos: Vector2):
 	Ui.gameplay_ui.hide_text()
 	navigation_agent.set_target_location(pos);
 	moving = true;
+	target_position = pos;
+	
+func play_or_continue_animation():
+	var angle = last_direction.angle() + PI;
+	var direction
+	var state
+	var flipped
+	
+	if(moving):
+		state = "walk"
+	else:
+		state = "idle"
+		
+	if(angle >= 2 * PI - PI / 8 or angle < 0 + PI / 8):
+		direction = "side"
+		flipped = true
+	elif(angle >= PI / 4 - PI / 8 and angle < PI / 4 + PI / 8):
+		direction = "side_up"
+		flipped = true
+	elif(angle >= PI / 2 - PI / 8 and angle < PI / 2 + PI / 8):
+		direction = "up"
+		flipped = false
+	elif(angle >= PI * 3/4 - PI / 8 and angle < PI * 3/4 + PI / 8):
+		direction = "side_up"
+		flipped = false
+	elif(angle >= PI - PI / 8 and angle < PI + PI / 8):
+		direction = "side"
+		flipped = false
+	elif(angle >= PI * 5/4 - PI / 8 and angle < PI * 5/4 + PI / 8):
+		direction = "side_down"
+		flipped = false
+	elif(angle >= PI * 6/4 - PI / 8 and angle < PI * 6/4 + PI / 8):
+		direction = "down";
+		flipped = true
+	elif(angle >= PI * 7/4 - PI / 8 and angle < PI * 7/4 + PI / 8):
+		direction = "side_down"
+		flipped = true;
+	
+	sprite.flip_h = flipped;
+	sprite.animation = state + "_" + direction
+	
 	
 func _physics_process(delta):
 	if(not moving):
+		play_or_continue_animation();
 		return;
 	
 	var move_direction = position.direction_to(navigation_agent.get_next_location());
+	last_direction = move_direction;
+	play_or_continue_animation();
 	velocity = move_direction * MAX_SPEED;
 	navigation_agent.set_velocity(velocity);
-	
+
 	if(navigation_agent.is_navigation_finished()):
 		if(target_object):
 			target_object._on_inspect();
-			
+		
+		last_direction = position.direction_to(target_position)
 		moving = false;
 	else:
 		velocity = move_and_slide(velocity);
